b40f7f029a3b17ed588916582d2c301bde9706ed
Waqas Ahmed <waqasahmed.uol@gmail.com>
2024-11-26 15:07:13 +0500

    Initial Commit

---
+++ Sources/AppButton/BannerImage.swift
@@ -0,0 +1,26 @@
+//
+//  BannerImage.swift
+//  AppButton
+//
+//  Created by waqas ahmed on 26/11/2024.
+//
+import Foundation
+import SwiftUI
+
+struct BannerImage: View {
+    var image: String
+    var height: CGFloat = 200
+    var width: CGFloat = 0
+    var paddingTop: CGFloat = 10
+    var cornerRadius: CGFloat = 25
+    var padding: CGFloat = 0
+    var body: some View {
+        Image(image)
+            .resizable()
+            .frame(width: width, height: height) // Specify only the height; width will adjust automatically
+            .cornerRadius(cornerRadius) // Apply corner radius
+            .padding(.horizontal, padding) // Apply horizontal padding to the image container
+            .padding(.top, paddingTop)
+            .clipped() // Clips any overflowing content to fit within the corner radius
+    }
+}
